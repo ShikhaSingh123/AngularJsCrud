f710b81b73b0cbaa79ace6de6bdde37f7d80c227
shikha bhadauriya <sbhadauriya@bhavnacorp.com>
2020-11-04 21:20:47 +0530

    Code changes

---
+++ AngularJsCrud/Models/Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace AngularJsCrud.Models
+{
+    public class Utility
+    {
+        public static string Encryptpassword(string password)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(12));
+            return hashedPassword;
+        }
+
+        public static string CheckPassword(string enteredPassword, string hashedPassword)
+        {
+            bool pwdHash = BCrypt.Net.BCrypt.Verify(enteredPassword, hashedPassword);
+            if(pwdHash== true)
+            {
+                return hashedPassword;
+            }
+             return hashedPassword;
+        }
+    }
+}
